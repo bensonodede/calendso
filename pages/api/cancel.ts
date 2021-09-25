89f284fb5e8bde076e5031376892de7655c7b1df
Syed Ali Shahbaz <alishahbaz7@gmail.com>
2021-09-25 17:36:34 +0530

    added cancel event hook and other minor additions --WIP

--- pages/api/cancel.ts
+++ pages/api/cancel.ts
@@ -1,13 +1,15 @@
 import prisma from "@lib/prisma";
-import { deleteEvent } from "@lib/calendarClient";
+import { CalendarEvent, deleteEvent } from "@lib/calendarClient";
 import { deleteMeeting } from "@lib/videoClient";
 import async from "async";
 import { BookingStatus } from "@prisma/client";
 import { asStringOrNull } from "@lib/asStringOrNull";
+import sendPayload from "@lib/webhooks/sendPayload";
+import getSubscriberUrls from "@lib/webhooks/subscriberUrls";
 
 export default async function handler(req, res) {
   // just bail if it not a DELETE
-  if (req.method !== "DELETE") {
+  if (req.method !== "DELETE" && req.method !== "POST") {
     return res.status(405).end();
   }
 
@@ -19,6 +21,11 @@ export default async function handler(req, res) {
     },
     select: {
       id: true,
+      userId: true,
+      title: true,
+      startTime: true,
+      endTime: true,
+      description: true,
       user: {
         select: {
           credentials: true,
@@ -31,12 +38,59 @@ export default async function handler(req, res) {
           type: true,
         },
       },
+      eventTypeId: true,
     },
   });
 
   if (!bookingToDelete) {
     return res.status(404).end();
   }
+
+  // const webhookEventTypes = await prisma.webhookEventTypes.findMany({
+  //   where: {
+  //     eventTypeId: parseInt(bookingToDelete.eventTypeId),
+  //   },
+  // });
+
+  // update organizer with proper organizer data like name, emailID, timeZone
+  const organizer = await prisma.user.findFirst({
+    where: {
+      id: bookingToDelete.userId as number,
+    },
+    select: {
+      name: true,
+      email: true,
+      timeZone: true,
+    },
+  });
+
+  // update type with proper event Type (event title fetched from eventType model)
+
+  const evt: CalendarEvent = {
+    type: bookingToDelete?.title,
+    title: bookingToDelete?.title,
+    description: bookingToDelete?.description || "",
+    startTime: bookingToDelete?.startTime.toString(),
+    endTime: bookingToDelete?.endTime.toString(),
+    organizer: organizer,
+    attendees: bookingToDelete?.attendees.map((attendee) => {
+      const retObj = { name: attendee.name, email: attendee.email, timeZone: attendee.timeZone };
+      return retObj;
+    }),
+  };
+
+  // Hook up the webhook logic here
+  const eventTrigger = "BOOKING_CANCELLED";
+  // Send Webhook call if hooked to BOOKING.CANCELLED
+  const subscriberUrls = await getSubscriberUrls(
+    bookingToDelete.userId,
+    bookingToDelete.eventTypeId,
+    eventTrigger
+  );
+
+  subscriberUrls.forEach((subscriberUrl: string) => {
+    sendPayload(eventTrigger, new Date().toISOString(), subscriberUrl, evt);
+  });
 
   // by cancelling first, and blocking whilst doing so; we can ensure a cancel
   // action always succeeds even if subsequent integrations fail cancellation.
